90766cac28afa5b15f37938abb2ca6b2704004b4
jeroenvdhoven <jeroenhoven@gmail.com>
2018-10-25 08:45:23 +0200

    Add clustering as a pipeline option

---
+++ R/clustering.R
@@ -0,0 +1,94 @@
+#' Add cluster labels to a training set
+#'
+#' @param train The train dataset, as a data.frame.
+#' @param cluster_column Name for the new cluster label column
+#' @param exclude_columns Columns to be excluded from the clustering. Should be provided as a character vector.
+#' Non-numerical columns will automatically be excluded
+#' @param k The number of clusters.
+#' @param metric The distance metric used. Currently only 'euclidean' and 'manhattan' are supported.
+#'
+#' @return A list containing the transformed train dataset, a .predict function to repeat the process on new data and all parameters needed to replicate the process.
+#' @export
+#'
+#' @importFrom cluster clara
+clustering <- function(train, cluster_column = "cluster", exclude_columns = character(0), k = 4,
+                           metric = "euclidean") {
+    stopifnot(
+        is.data.frame(train),
+        is.character(exclude_columns),
+        !any(!exclude_columns %in% colnames(train)),
+        is.character(cluster_column),
+        !cluster_column %in% colnames(train),
+        is.character(metric),
+        metric %in% c("euclidean", "manhattan"),
+        is.numeric(k)
+    )
+
+    if(length(exclude_columns) > 0) train_cluster <- select_(train, .dots = paste0("-", exclude_columns))
+    else train_cluster <- train
+
+    # Make sure non-numeric columns are added to excluded columns
+    non_numeric_columns <- !purrr::map_lgl(.x = train_cluster, .f = ~ is.numeric(.) || is.logical(.))
+    if(any(non_numeric_columns)) {
+        extra_excluded_columns <- colnames(train_cluster)[non_numeric_columns]
+        train_cluster <- select_(train_cluster, .dots = paste0("-", extra_excluded_columns))
+        exclude_columns <- unique(c(extra_excluded_columns, exclude_columns))
+    }
+
+    stopifnot(
+        !any(!purrr::map_lgl(.x = train_cluster, .f = ~ is.numeric(.) || is.logical(.))),
+        ncol(train_cluster) > 0
+    )
+
+    clustering <- cluster::clara(x = train_cluster, k = k, metric = metric, keep.data = F,
+                                 correct.d = T,
+                                 samples = ceiling(sqrt(nrow(train_cluster))), pamLike = T)
+
+
+    train[cluster_column] <- as.character(clustering$clustering)
+    clustering$medoids <- as_data_frame(clustering$medoids)
+
+    predict_function <- function(data) {
+        clustering_predict(data = data, metric = metric, centroids = clustering$medoids,
+                               exclude_columns = exclude_columns, cluster_column = cluster_column)
+    }
+
+    return(list(train = train, .predict = predict_function, centroids = clustering$medoids,
+                exclude_columns = exclude_columns, metric = metric, cluster_column = cluster_column))
+}
+
+clustering_predict <- function(data, metric, centroids, cluster_column, exclude_columns) {
+    stopifnot(
+        is.data.frame(data),
+        is.character(exclude_columns),
+        !any(!exclude_columns %in% colnames(data)),
+        is.character(cluster_column),
+        !cluster_column %in% colnames(data),
+        is.character(metric),
+        metric %in% c("euclidean", "manhattan"),
+        is.data.frame(centroids)
+    )
+
+    if(length(exclude_columns) > 0) data_cluster <- select_(data, .dots = paste0("-", exclude_columns))
+    else data_cluster <- data
+
+    stopifnot(ncol(centroids) == ncol(data_cluster))
+
+    if(metric == "euclidean") dist_func <- function(center, points) {
+        individual_dists <- purrr::map2_df(.x = center, .y = points, .f = function(x,y) (x-y) ^ 2)
+        dists <- apply(individual_dists, 1, function(x) sqrt(sum(x, na.rm = T)))
+        return(dists)
+    } else if(metric == "manhattan") dist_func <- function(center, points) {
+        individual_dists <- purrr::map2_df(.x = center, .y = points, .f = function(x,y) abs(x-y))
+        dists <- apply(individual_dists, 1, function(x) sum(x, na.rm = T))
+        return(dists)
+    }
+
+    clusters <- apply(centroids, 1, dist_func, points = data_cluster) %>%
+        apply(1, which.min)
+
+    stopifnot(length(clusters) == nrow(data_cluster))
+
+    data[cluster_column] <- as.character(clusters)
+    return(data)
+}
---
+++ tests/testthat/test_clustering.R
@@ -0,0 +1,58 @@
+describe("clustering()", {
+    na_columns <- colnames(dataset1)[purrr::map_lgl(dataset1, anyNA)]
+
+    it("runs without errors on basic settings", {
+        r <- ctest_for_no_errors(error_message = "clustering() does not run on defaults",
+                                 to_eval = clustering(train = dataset1))
+        r <- ctest_for_no_errors(error_message = "clustering() does not run on defaults with exclusion",
+                                 to_eval = clustering(train = dataset1, exclude_columns = na_columns))
+    })
+
+    it("returns a list with at least train and .predict names, where the first is a dataset and the second a function", {
+        r <- clustering(train = dataset1, exclude_columns = na_columns)
+
+        ctest_pipe_has_correct_fields(r)
+    })
+
+    it("can apply its results to a new dataset using .predict, a wrapper for clustering_predict()", {
+        r <- clustering(train = dataset1, exclude_columns = na_columns)
+        ctest_pipe_has_working_predict_function(pipe_res = r, data = dataset1)
+    })
+
+    it("can run when not excluding missing values or non-numeric columns", {
+        r <- clustering(train = dataset1, exclude_columns = character(0))
+    })
+
+    it("can run when not excluding missing values or non-numeric columns on new data", {
+        r <- clustering(train = dataset1, exclude_columns = character(0))
+        ctest_pipe_has_working_predict_function(pipe_res = r, data = dataset1)
+    })
+
+    it("adds cluster labels to a dataset as a character vector", {
+        clust_column <- "cluster"
+        test_df <- data_frame(
+            x = c(rnorm(n = 100), rnorm(n = 100, mean = 10)),
+            y = c(rnorm(n = 100, mean = -1, sd = 3), rnorm(n = 100, mean = 6, sd = 1))
+        )
+
+        r <- clustering(train = test_df, cluster_column = clust_column, k = 2)
+
+        expect_true(clust_column %in% colnames(r$train))
+
+        col <- unlist(r$train[clust_column])
+        expect_true(is.character(col))
+        expect_equivalent(col, c(rep("1", 100), rep("2", 100)))
+    })
+
+    it("allows you to set the number of clusters", {
+        k <- 9
+        r <- clustering(train = dataset1, k = k)
+
+        expect_equal(max(as.numeric(r$train$cluster)), k)
+    })
+
+    it("allows you to set the distance metric", {
+        r <- ctest_for_no_errors(error_message = "clustering() does not run on defaults",
+                            to_eval = clustering(train = dataset1, metric = "manhattan"))
+    })
+})
