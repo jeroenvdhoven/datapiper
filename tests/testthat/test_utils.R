943edee2b5df98285414b08c23cc33dc7e87988f
jeroenvdhoven <jeroenhoven@gmail.com>
2019-01-28 20:21:47 +0100

    Add extra tests for utils

--- tests/testthat/test_utils.R
+++ tests/testthat/test_utils.R
@@ -42,3 +42,74 @@ describe("standard_column_names", {
         expect_named(transformed, c("spa_ced"))
     })
 })
+
+
+describe("select_cols", {
+    it("will select columns for both data.tables and data.frames in the same way", {
+        cols <- c("x", "y")
+
+        r_df <- select_cols(data = dataset1, cols = cols)
+        r_dt <- as_data_frame(select_cols(data = as.data.table(dataset1), cols = cols))
+
+        expect_equal(expected = cols, colnames(r_df))
+        expect_equal(expected = cols, colnames(r_dt))
+        expect_equal(expected = nrow(r_df), nrow(dataset1))
+        expect_equal(expected = nrow(r_dt), nrow(dataset1))
+        expect_equal(r_dt, r_df)
+    })
+
+    it("will subset rows for both data.tables and data.frames in the same way", {
+        rows <- seq_len(N / 2)
+
+        r_df <- select_cols(data = dataset1, rows = rows)
+        r_dt <- as_data_frame(select_cols(data = as.data.table(dataset1), rows = rows))
+
+        expect_equal(expected = colnames(dataset1), colnames(r_df))
+        expect_equal(expected = colnames(dataset1), colnames(r_dt))
+        expect_equal(expected = length(rows), nrow(r_df))
+        expect_equal(expected = length(rows), nrow(r_dt))
+        expect_equal(r_dt, r_df)
+    })
+
+    it("will subset rows and columns for both data.tables and data.frames in the same way", {
+        cols <- c("x", "y")
+        rows <- seq_len(N / 2)
+
+        r_df <- select_cols(data = dataset1, rows = rows, cols = cols)
+        r_dt <- as_data_frame(select_cols(data = as.data.table(dataset1), rows = rows, cols = cols))
+
+        expect_equal(expected = cols, colnames(r_df))
+        expect_equal(expected = cols, colnames(r_dt))
+        expect_equal(expected = length(rows), nrow(r_df))
+        expect_equal(expected = length(rows), nrow(r_dt))
+        expect_equal(r_dt, r_df)
+    })
+})
+
+describe("deselect_cols", {
+    it("can remove columns from both data.tables and data.frames in the same way", {
+        cols <- c("x", "y")
+
+        r_df <- deselect_cols(data = dataset1, cols = cols)
+        r_dt <- as_data_frame(deselect_cols(data = as.data.table(dataset1), cols = cols, inplace = F))
+
+        expected_columns <- colnames(dataset1)[!colnames(dataset1) %in% cols]
+        expect_equal(expected = expected_columns, colnames(r_df))
+        expect_equal(expected = expected_columns, colnames(r_dt))
+        expect_equal(expected = nrow(dataset1), nrow(r_df))
+        expect_equal(expected = nrow(dataset1), nrow(r_dt))
+        expect_equal(r_dt, r_df)
+    })
+
+    it("can remove columns from data.tables by reference", {
+        cols <- c("x", "y")
+
+        tmp <- as.data.table(dataset1)
+        r_dt <- deselect_cols(data = tmp, cols = cols, inplace = T)
+
+        expected_columns <- colnames(dataset1)[!colnames(dataset1) %in% cols]
+        expect_equal(expected = expected_columns, colnames(r_dt))
+        expect_equal(expected = nrow(dataset1), nrow(r_dt))
+        expect_equal(r_dt, tmp)
+    })
+})
